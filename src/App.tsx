874f55f3bf455e3f046eff4b2bd3f88ad30d8726
ath92 <tom@ath92.nl>
2019-11-01 17:07:08 +0100

    merge gain and gainDestination prop into one

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import OscillatorNode from './components/web-audio/OscillatorNode';
-import AudioContextNode from './components/web-audio/AudioContextNode';
-import GainNode from './components/web-audio/GainNode';
-import BiQuadFilterNode from './components/web-audio/BiQuadFilterNode';
+import React, { useState } from "react";
+import OscillatorNode from "./components/web-audio/OscillatorNode";
+import AudioContextNode from "./components/web-audio/AudioContextNode";
+import GainNode from "./components/web-audio/GainNode";
+import BiQuadFilterNode from "./components/web-audio/BiQuadFilterNode";
 
-import GainOscillator from './components/ui/GainOscillator';
+import GainOscillator from "./components/ui/GainOscillator";
 
 const App: React.FC = () => {
   const [makeNoise, setMakeNoise] = useState(false);
@@ -18,7 +18,7 @@ const App: React.FC = () => {
           <BiQuadFilterNode type="lowpass">
             <OscillatorNode type="square" frequency={baseFrequency} />
           </BiQuadFilterNode>
-          <GainNode gain={0.5} gainDestination={ <OscillatorNode type="sine" frequency={5} /> }>
+          <GainNode gain={<GainOscillator />}>
             <OscillatorNode type="sawtooth" frequency={baseFrequency * 3} />
             <OscillatorNode type="sine" frequency={baseFrequency * 4} />
           </GainNode>
@@ -26,15 +26,17 @@ const App: React.FC = () => {
         <GainOscillator />
       </AudioContextNode>
     );
-  }
+  };
 
   return (
     <React.Fragment>
       <button onClick={() => setMakeNoise(!makeNoise)}>Make some noise!</button>
-      <button onClick={() => setBaseFrequency(baseFrequency * 2)}>Higher</button>
-      { renderSynth() }
+      <button onClick={() => setBaseFrequency(baseFrequency * 2)}>
+        Higher
+      </button>
+      {renderSynth()}
     </React.Fragment>
   );
-}
+};
 
 export default App;
--- src/components/web-audio/GainNode.tsx
+++ src/components/web-audio/GainNode.tsx
@@ -1,40 +1,46 @@
-import React, { FC, useContext, useEffect, useState } from 'react';
-import DestinationContext from '../../context/DestinationContext';
-import AudioContextContext from '../../context/AudioContextContext';
+import React, { FC, useContext, useEffect, useState } from "react";
+import DestinationContext from "../../context/DestinationContext";
+import AudioContextContext from "../../context/AudioContextContext";
 
 interface Props {
-    gain: number,
-    gainDestination?: any
+  gain: number | JSX.Element;
 }
 
 const GainNode: FC<Props> = props => {
-    const audioCtx = useContext(AudioContextContext);
-    const destination = useContext(DestinationContext);
-    const [gainNode, setGainNode] = useState<GainNode>(audioCtx.createGain());
+  const audioCtx = useContext(AudioContextContext);
+  const destination = useContext(DestinationContext);
+  const [gainNode, setGainNode] = useState<GainNode>(audioCtx.createGain());
 
-    // connect node
-    useEffect(() => {
-        const node = audioCtx.createGain();
-        node.connect(destination);
-        setGainNode(node);
-        return () => node.disconnect(destination);
-    }, [destination, audioCtx]);
+  // connect node
+  useEffect(() => {
+    const node = audioCtx.createGain();
+    node.connect(destination);
+    setGainNode(node);
+    return () => node.disconnect(destination);
+  }, [destination, audioCtx]);
 
-    // handle updates to gain
-    useEffect(() => {
-        gainNode.gain.value = props.gain;
-    }, [props.gain, gainNode]);
+  // handle updates to gain
+  useEffect(() => {
+    if (typeof props.gain === "number") {
+      gainNode.gain.value = props.gain;
+    }
+  }, [props.gain, gainNode]);
 
-    return (
-        <React.Fragment>
-            <DestinationContext.Provider value={ gainNode }>
-                { props.children }
-            </DestinationContext.Provider>
-            <DestinationContext.Provider value={ gainNode.gain }>
-                { props.gainDestination }
-            </DestinationContext.Provider>
-        </React.Fragment>
-    );
-}
+  const gainDestinationProvider =
+    typeof props.gain !== "number" ? (
+      <DestinationContext.Provider value={gainNode.gain}>
+        {props.gain}
+      </DestinationContext.Provider>
+    ) : null;
+
+  return (
+    <React.Fragment>
+      <DestinationContext.Provider value={gainNode}>
+        {props.children}
+      </DestinationContext.Provider>
+      {gainDestinationProvider}
+    </React.Fragment>
+  );
+};
 
 export default GainNode;
